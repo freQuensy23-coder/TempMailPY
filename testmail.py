cbea3dc672d5e0e4ae5241b8dc7650c8b004c972
XazkerBoy <62183587+XazkerBoy@users.noreply.github.com>
2020-03-17 21:39:42 +0100

    Update
    Error checking added

--- testmail.py
+++ testmail.py
@@ -9,24 +9,33 @@ mailclient = NADA()		#Init NADA client and get new mail address
 print(mailclient.getData())		#Display mail address
 
 while True:
-	answer = input('[1] - New email address\r\n[2] - Check new (+old) messages\r\n[3] - Get all messages\r\nSelect action: ')
+	answer = input('[1] - New email address\r\n[2] - Refresh messages\r\n[3] - Get already fetched messages\r\nSelect action: ')
 	if answer == '1':
-		mailclient.newEmail()	#Get new mail address
+		resp = mailclient.newEmail()	#Get new mail address
+		if resp != None:	#Check email creation errors
+			print(resp)
+			continue
 		print(mailclient.getData())
 	elif answer == '2':
 		messages = mailclient.getMessages()	#Get messages
+		if type(messages) == 'str':
+			print(messages)
+			continue
 		print('Message count: ' + str(messages[1]))	#Print message count
-		if(messages[1] > 0):
+		if messages[1] > 0:
 			for message in messages[0]:	#Display each message
 				print('Message info:')
 				print(message)
 				print('Message body:')
 				print(mailclient.getMessage(message[0]))
 	elif answer == '3':
-		messages = mailclient.getAll()	#Get all messages
-		print('Message count: ' + str(len(messages)))
-		if(len(messages) > 0):
-			for message in messages:
+		messages = mailclient.getAll()	#Get fetched messages
+		if type(messages) == 'str':
+			print(messages)
+			continue
+		print('Message count: ' + str(messages[1]))	#Print message count
+		if messages[1] > 0:
+			for message in messages[0]:	#Display each message
 				print('Message info:')
 				print(message)
 				print('Message body:')
